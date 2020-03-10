a41f2eafb7352d51fe09fb0eca41b58461a3b605
Lova Wins <lovaunger@gmail.com>
2020-03-10 14:10:03 +0100

    added id

--- App.vue
+++ App.vue
@@ -1,16 +1,19 @@
 <template>
+<div class="wrapper">
   <div id="app">
     <h1>{{title}}</h1>
     <input type="text" placeholder="Add todo..." v-on:keyup.enter="addTodo" />
-
     <ul>
       <li
         v-for="(todo, index) in todos"
         v-bind:key="index"
         v-on:click="todo.isCompleted = !todo.isCompleted"
         :class="{ strikeThrough: todo.isCompleted }"
-      >{{todo.item}}</li>
+      >{{todo.item}}
+      <button class="nes-btn is-error padding">X</button>
+      </li>
     </ul>
+  </div>
   </div>
 </template>
 <script>
@@ -18,10 +21,10 @@ export default {
   data: () => ({
     title: "Lovas Todos",
     todos: [
-      { item: "Drink coffee", isCompleted: false },
-      { item: "Code", isCompleted: false },
-      { item: "Cry", isCompleted: false },
-      { item: "Repeat", isCompleted: false }
+      { item: "Drink coffee", isCompleted: false, id: Date.now()},
+      { item: "Code", isCompleted: false, id: Date.now() },
+      { item: "Cry", isCompleted: false, id: Date.now() },
+      { item: "Repeat", isCompleted: false, id: Date.now() }
     ]
   }),
 
@@ -34,8 +37,12 @@ export default {
 
     addTodo(event) {
       const item = event.target.value;
-      this.todos.push({ item, isCompleted: false });
+      this.todos.push({ item, isCompleted: false, id: Date.now() });
       event.target.value = "";
+    },
+
+    removeTodo(){
+
     }
   }
 };
@@ -43,12 +50,20 @@ export default {
 
 
 <style>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #ffa2d0;
+}
+
 #app {
   font-family: courier, monospace;
 }
 
 h1 {
   font-family: "press start 2p";
+  font-size: 3rem;
 }
 
 .strikeThrough {
@@ -66,13 +81,47 @@ h1 {
 
 .strike {
   text-decoration: line-through;
+  transition: 3s;
 }
 
 body {
-  background-color: #ffa2d0;
+  background-color: #222;
 }
 
 li:hover {
   cursor: pointer;
+}
+
+input {
+  background: #222;
+  border:none;
+  border-bottom: solid 2px #ffa2d0;
+  width: 10rem;
+  height: 2rem;
+  font-family: 'courier new';
+  font-size: 1.3rem;
+  color: #ffa2d0;
+}
+
+li {
+  font-size: 2rem;
+}
+
+::placeholder {
+  font-family: 'courier new';
+  font-size: 1.2rem;
+}
+
+.nes-btn {
+
+  border: none;
+  color: #ffa2d0;
+  font-size: 1rem;
+  background: #222;
+}
+
+
+.nes-btn:hover {
+  cursor:pointer;
 }
 </style>
